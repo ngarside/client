f64fd99cb1f05b16f0d9f8bbc11094778f5e3209
Nathan Garside <git@nathangarside.com>
2025-08-26 19:20:04 +0100

    Install the github cli

---
+++ common/build/08-github.sh
@@ -0,0 +1,14 @@
+# This is free and unencumbered software released into the public domain.
+
+rpm --import https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x23F3D4EA75716059
+
+cat > /etc/yum.repos.d/gh-cli.repo <<EOF
+[gh-cli]
+name=GitHub CLI
+baseurl=https://cli.github.com/packages/rpm
+enabled=1
+gpgcheck=1
+gpgkey=https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x23F3D4EA75716059
+EOF
+
+dnf --assumeyes install gh --repo gh-cli
