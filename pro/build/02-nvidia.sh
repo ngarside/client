76fc618d015e11c9260c3490fe435915804ebbe8
Nathan Garside <git@nathangarside.com>
2025-08-26 16:43:23 +0100

    Don't hardcode fedora version (#11)

--- pro/build/02-nvidia.sh
+++ pro/build/02-nvidia.sh
@@ -1,13 +1,14 @@
 # This is free and unencumbered software released into the public domain.
 
+KERNEL=$(rpm -qa kernel | grep -oP '(?<=kernel-).*')
+OS=$(rpm -qa kernel | grep -oP 'fc.*')
+
 dnf --assumeyes install akmod-nvidia xorg-x11-drv-nvidia
 
 echo "%_with_kmod_nvidia_open 1" > /etc/rpm/macros.nvidia-kmod
-
-KERNEL=$(rpm -qa kernel | grep -oP '(?<=kernel-).*')
 
 akmods --force --kernels $KERNEL --rebuild
 
 tree /var/cache
 
-dnf --assumeyes install /var/cache/akmods/nvidia/kmod-nvidia-$KERNEL-575.64.05-2.fc42.x86_64.rpm
+dnf --assumeyes install /var/cache/akmods/nvidia/kmod-nvidia-$KERNEL-575.64.05-2.$OS.rpm
