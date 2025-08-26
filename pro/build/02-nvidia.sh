d3e1e11bd19355b846f8a199be13dd2911809ecf
Nathan Garside <git@nathangarside.com>
2025-08-26 19:34:22 +0100

    Support updated drivers

--- pro/build/02-nvidia.sh
+++ pro/build/02-nvidia.sh
@@ -1,14 +1,13 @@
 # This is free and unencumbered software released into the public domain.
 
-KERNEL=$(rpm -qa kernel | grep -oP '(?<=kernel-).*')
-OS=$(rpm -qa kernel | grep -oP 'fc.*')
-
 dnf --assumeyes install akmod-nvidia xorg-x11-drv-nvidia
 
 echo "%_with_kmod_nvidia_open 1" > /etc/rpm/macros.nvidia-kmod
 
+KERNEL=$(rpm -qa kernel | grep -oP '(?<=kernel-).*')
+
 akmods --force --kernels $KERNEL --rebuild
 
-tree /var/cache
+PACKAGE=$(find /var/cache/akmods/nvidia -type f -name kmod*)
 
-dnf --assumeyes install /var/cache/akmods/nvidia/kmod-nvidia-$KERNEL-575.64.05-2.$OS.rpm
+dnf --assumeyes install $PACKAGE
