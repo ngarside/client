dae11f28213b92bd91f0cc471786eedb7fff007c
Nathan Garside <git@nathangarside.com>
2025-08-26 15:12:37 +0100

    Install chezmoi

---
+++ common/build/07-chezmoi.sh
@@ -0,0 +1,4 @@
+# This is free and unencumbered software released into the public domain.
+
+dnf --assumeyes install \
+    https://github.com/twpayne/chezmoi/releases/download/v2.65.0/chezmoi-2.65.0-x86_64.rpm
