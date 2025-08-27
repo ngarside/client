89da15f9199c873a0ec4603668d6c60e32677168
Nathan Garside <git@nathangarside.com>
2025-08-27 10:35:49 +0100

    Enable secure boot (#20)

--- pro/build/02-nvidia.sh
+++ pro/build/02-nvidia.sh
@@ -1,12 +1,27 @@
+#!/usr/bin/env bash
 # This is free and unencumbered software released into the public domain.
+
+set -euo pipefail
 
 dnf --assumeyes install akmod-nvidia xorg-x11-drv-nvidia
 
-echo "%_with_kmod_nvidia_open 1" > /etc/rpm/macros.nvidia-kmod
+mkdir /tmp/pki
+echo -e "$KMOD_PRIVATEKEY" > /tmp/pki/kmod_privatekey.rsa
+echo -e "$KMOD_PUBLICKEY" > /tmp/pki/kmod_publickey.rsa
+
+openssl x509 -in /tmp/pki/kmod_publickey.rsa -outform der -out /tmp/pki/kmod_publickey.der
+
+cat > /etc/rpm/macros.nvidia-kmod <<EOF
+%_kmodtool_signmodules_privkey /tmp/pki/kmod_privatekey.rsa
+%_kmodtool_signmodules_pubkey /tmp/pki/kmod_publickey.der
+%_with_kmod_nvidia_open 1
+EOF
 
 KERNEL=$(rpm -qa kernel | grep -oP '(?<=kernel-).*')
 
 akmods --force --kernels $KERNEL --rebuild
+
+rm --force --recursive /tmp/pki
 
 PACKAGE=$(find /var/cache/akmods/nvidia -type f -name kmod*)
 
