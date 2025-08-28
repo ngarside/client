0a9365c37c2ee3683cc15f33c21901e23c1f8a9a
Nathan Garside <git@nathangarside.com>
2025-08-28 12:44:15 +0100

    Use static lighting for memory (#25)
    * Auto apply rgb lighting
    
    * Don't reload systemd daemon

--- pro/build/04-openrgb.sh
+++ pro/build/04-openrgb.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 # This is free and unencumbered software released into the public domain.
+# Note that the ordering of options passed to OpenRGB is significant - don't modify it.
 
 set -euo pipefail
 
 dnf --assumeyes install openrgb
+
+cat > /etc/systemd/system/openrgb.service <<EOF
+[Unit]
+Description=Apply default lighting configuration using OpenRGB
+
+[Service]
+ExecStart=openrgb --device 'ENE DRAM' --color white --mode static
+RemainAfterExit=yes
+RuntimeDirectory=openrgb
+Type=oneshot
+WorkingDirectory=/run/openrgb
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable openrgb
