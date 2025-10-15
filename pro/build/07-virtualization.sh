01f7df9a5e69d58d2e069dbba37a3d03feced457
Nathan Garside <git@nathangarside.com>
2025-10-15 09:58:38 +0100

    Allow access to libvirt (#47)

--- pro/build/07-virtualization.sh
+++ pro/build/07-virtualization.sh
@@ -4,3 +4,12 @@
 set -euo pipefail
 
 dnf --assumeyes group install --with-optional virtualization
+
+cat > /etc/polkit-1/localauthority/50-local.d/10-libvirt-access-kvm-group.pkla <<EOF
+[Access libvirt from kvm group]
+Identity=unix-group:kvm
+Action=org.libvirt.unix.manage
+ResultAny=yes
+ResultInactive=yes
+ResultActive=yes
+EOF
