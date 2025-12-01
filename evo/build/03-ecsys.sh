cca86ba7e8f7d07dd3c0081b5a4933266f1ffe8e
Nathan Garside <git@nathangarside.com>
2025-12-01 16:19:43 +0000

    Build EC SYS in own stage

--- evo/build/03-ecsys.sh
+++ evo/build/03-ecsys.sh
@@ -3,24 +3,5 @@
 
 set -euo pipefail
 
-dnf --assumeyes copr enable ferdiu/kernel-modules
-dnf --assumeyes install \
-	asciidoc audit-libs-devel bindgen binutils-devel bpftool clang clippy \
-	dwarves elfutils-devel fuse-devel gcc-c++ glibc-static java-devel \
-	kernel-rpm-macros koji libbabeltrace-devel libbpf-devel libcap-devel \
-	libcap-ng-devel libmnl-devel libnl3-devel libtraceevent-devel \
-	libtracefs-devel libxml2-devel lld llvm-devel ncurses-devel net-tools \
-	newt-devel nss-tools numactl-devel pciutils-devel perl perl-devel \
-	perl-generators pesign python3-devel python3-docutils python3-pip \
-	python3-setuptools python3-setuptools rustfmt rust-src swig \
-	systemd-boot-unsigned systemd-ukify xmlto xz-devel
-
-set +e
-dnf --assumeyes install ec_sys-kmod
-set -e
-
-KERNEL=$(rpm -qa kernel | grep -oP '(?<=kernel-).*')
-akmods --force --kernels $KERNEL --rebuild
-
-PACKAGE=$(find /var/cache/akmods/ec_sys -type f -name kmod*)
+PACKAGE=$(find /tmp/ec-sys -type f -name kmod*)
 dnf --assumeyes install $PACKAGE
