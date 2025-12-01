45c716dba9c5bced8126626ec2ae7caedbd21cd5
Nathan Garside <git@nathangarside.com>
2025-12-01 09:26:42 +0000

    Install MSI EC

---
+++ evo/build/01-msiec.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This is free and unencumbered software released into the public domain.
+
+set -euo pipefail
+
+dnf --assumeyes install dkms kernel-devel
+
+git clone https://github.com/ngarside/msi-ec.git \
+	--branch feature/prestige-13-a1mg \
+	--depth 1 \
+	/tmp/msi-ec
+cd /tmp/msi-ec
+
+KERNEL=$(rpm -qa kernel | grep -oP '(?<=kernel-).*')
+make KERNELRELEASE=$KERNEL
+make KERNELRELEASE=$KERNEL dkms-install
+
+rm --recursive /tmp/msi-ec
