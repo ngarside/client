0fc305faeb66d4b27ec9872f981d98c0b206eb7a
Nathan Garside <git@nathangarside.com>
2026-01-11 08:46:21 +0000

    Install chezmoi from fedora

--- common/build/07-chezmoi.sh
+++ common/build/07-chezmoi.sh
@@ -3,8 +3,4 @@
 
 set -euo pipefail
 
-ARCH=$(rpm -qa kernel | grep -oP '(?<=\.)[^.]*$')
-VERSION='2.65.0'
-
-dnf --assumeyes install \
-	https://github.com/twpayne/chezmoi/releases/download/v$VERSION/chezmoi-$VERSION-$ARCH.rpm
+dnf --assumeyes install chezmoi
