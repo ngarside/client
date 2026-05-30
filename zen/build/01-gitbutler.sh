75dd2443c22e31eb4989a933c41ccf4acf20fe6d
Nathan Garside <git@nathangarside.com>
2026-05-30 10:23:40 +0100

    Remove git butler (#83)

--- zen/build/01-gitbutler.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This is free and unencumbered software released into the public domain.
-
-set -euo pipefail
-
-dnf --assumeyes install https://releases.gitbutler.com/releases/release/0.19.9-2990/linux/x86_64/GitButler-0.19.9-1.x86_64.rpm
